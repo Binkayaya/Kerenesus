1d7aa9e5011b9b90f3d73d26de5a2da4978b4820
Binkayaya <binkayaya@gmail.com>
2025-01-18 02:11:36 +0100

    This is a test project. Here I developed a numbers sorting program. A user enters the numbers they want sorted, specifies the sorting order, and gets the result.

--- SortNumbers.py
+++ SortNumbers.py
@@ -1,4 +1,3 @@
-var_num = 87
 max_num = int(input('How many numbers do you want to sort? '))
 sort_choice = str(input('How do you want to sort? Type A for ascending and D for descending order: '))
 
