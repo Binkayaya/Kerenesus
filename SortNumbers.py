c2b047f8febf833cac6f93afdb21c3a04e85c4b0
Binkayaya <binkayaya@gmail.com>
2025-01-19 01:19:56 +0100

    Peace's idea for inputing numbers in a single list instead of using Enter multiple times

--- SortNumbers.py
+++ SortNumbers.py
@@ -1,29 +1,28 @@
-var_num = 78
-max_num = int(input('How many numbers do you want to sort? '))
 sort_choice = str(input('How do you want to sort? Type A for ascending and D for descending order: '))
 
-num_list = []
 
 if sort_choice == 'A' or sort_choice == 'D':
-    for i in range(1, max_num+1):
-        number = int(input(f'Enter number {i}: '))
-        num_list.append(number)
+    init_num = input('Paste the numbers you want to sort (separate with commas):  ')
+    tup_num = tuple(map(int, init_num.split(",")))
+    num_list = list(tup_num)
+    
+    
+    sorted_list = []
+    len_num = len(num_list)
+    
+    while len(sorted_list) < len_num:
+        if sort_choice == 'A':
+            sorted_list.append(min(num_list))
+            num_list.remove(min(num_list))
+        
+        else:
+            sorted_list.append(max(num_list))
+            num_list.remove(max(num_list))
+
+    if len(sorted_list) > 0:
+        print(sorted_list)
+   
 else:
     print('You have not made a valid choice')
-
-sorted_list = []
-len_num = len(num_list)
-
-while len(sorted_list) < len_num:
-    if sort_choice == 'A':
-        sorted_list.append(min(num_list))
-        num_list.remove(min(num_list))
-    
-    else:
-        sorted_list.append(max(num_list))
-        num_list.remove(max(num_list))
-
-if len(sorted_list) > 0:
-    print(sorted_list)
 
 
